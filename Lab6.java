74e9cd40175f74d4ae19d73387e1d7e1d8244c64
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-09-23 02:44:56 +0000

    Lab6_template

---
+++ Lab6.java
@@ -0,0 +1,49 @@
+/*
+ *   write your header comments here
+ */
+
+//as you write your code, be sure to remove the comment delimiters as you go!
+
+public class Lab6{
+
+// given two integers x and y, return their average.
+   //  public static double average (int x, int y)
+   //  {
+
+   //  }
+
+     // given two integers, x and y, return the difference x - y  (notice, order matters!)
+   //  public static int difference (int x, int y)
+   //  {
+
+   //  }
+
+     //given an integer x, return its suare, x * x
+   //  public static int square (int x)
+    // {
+
+   //  }
+
+     //given two points (x1, y1) and (x2, y2), return 
+     //the slope of the line through them.  You may assume x1 is not equal to x2
+   //  public static double slope (int x1, int y1, int x2, int y2)
+   //  {
+
+    // }
+
+     // given two points (x1, y1) and (x2, y2), return the distance between them.  
+   //  public static double distance(int x1, int y1, int x2, int y2)
+   //  {
+
+    // }
+
+
+//main driver method
+//this method will include the output statements.
+public static void main(String[] args){
+          //write your main code here
+     
+
+
+}  //end main driver method
+}//end class 
